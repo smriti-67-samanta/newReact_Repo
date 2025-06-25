f6778a12161fda737bb95289c87659940ca684e9
unknown <samantasmriti0@gmail.com>
2025-06-25 19:13:31 +0530

    counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Counter from './components/Counter'
+import ToggleButton from './components/ToggleButton';
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Counter/>
+     <div> <ToggleButton/></div>
      
     </>
   )
